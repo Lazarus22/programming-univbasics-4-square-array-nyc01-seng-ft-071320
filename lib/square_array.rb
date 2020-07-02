edc036f23aff8422201ab18ff225a88009176691
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-02 19:17:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
-def square_array(array)
-  array = [1,2,3,4,5]
+array = [1,2,3,4,5]
 new_array = []
 
+def square_array(array)
 array.length.times do |index|
   new_array.push(array[index] ** 2)
 end
-puts new_array
+new_array
